f2985d1125994236fe919f532ac8bf81c6d91eac
Grecia Cardona <crimsondestiny96@gmail.com>
2022-12-14 15:46:06 -0600

    Symptoms in buttons

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import DoctorImg from '../../images/doctor.svg';
 import './Banner.css';
+import Button from 'react-bootstrap/Button';
 
 export const Banner = () => { 
   return (
@@ -22,6 +23,40 @@ export const Banner = () => {
                 </div>
             </div>
         </div>
+        <div>
+            <div className='row'>
+                <div className='col-5'>
+                    <Button variant="outline-primary">Piel enrojecida</Button>{' '}
+                    <Button variant="outline-secondary">Estornudos</Button>{' '}
+                    <Button variant="outline-success">Dolor al orinar</Button>{' '}
+                    <Button variant="outline-warning">Dolor de cabeza</Button>{' '}
+                </div>
+            <div className='row'>
+                <div className='col-5'>
+                    <Button variant="outline-primary">Dolor palpitante cabeza</Button>{' '}
+                    <Button variant="outline-secondary">Orinar con frecuencia</Button>{' '}
+                    <Button variant="outline-success">Comezón en la piel</Button>{' '}
+                    <Button variant="outline-warning">Moqueo</Button>{' '}
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-5'>
+                    <Button variant="outline-primary">Tos frecuente</Button>{' '}
+                    <Button variant="outline-secondary">Piel sensible</Button>{' '}
+                    <Button variant="outline-success">Sangre en orina</Button>{' '}
+                    <Button variant="outline-warning">Sensibilidad a luz</Button>{' '}
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-5'>
+                    <Button variant="outline-primary">Nauseas </Button>{' '}
+                    <Button variant="outline-secondary">Presión en el abdomen</Button>{' '}
+                    <Button variant="outline-success">Piel inflamada</Button>{' '}
+                    <Button variant="outline-warning">Lagrimeo</Button>{' '}
+                </div>
+            </div>
+        </div>
     </div>
-  )
+    </div>
+  );
 }
