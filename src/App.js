4bb2bc359f0944e8f87c7fb09d0f0bd25e5120fa
AlexxCepeda <acepeda1800@alumno.ipn.mx>
2022-12-14 01:37:06 -0600

    about

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Banner } from './components/Banner/Banner';
 import { Footer } from './components/Footer/Footer';
 import { Navbar } from './components/Navbar/Navbar';
-
+import { About } from './components/pages/About';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Banner/>
-      <Footer/>
-      
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Banner />}></Route>
+        <Route path='/about' element={<About />} />
+      </Routes>
+      <Footer />
+
     </div>
   );
 }
